15e56fdea07e88cfeee8695319c6ca6737f61917
Joshua Zuke <zuke.joshua@gmail.com>
2021-02-23 22:41:10 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -9,7 +9,8 @@ def turn_count(board)
 end
 
 def current_player(board)
-  if turn_count(board) % 2 == 0
+  if turn_count(board) % 2 == 0 # Note: This operator shows the remainder when divided by a given number. It's another way of saying the number is even (R=0 when divided by 2).
+  # Alternatively, if turn_count(board).even? then "X"
     "X"
   else
     "O"
